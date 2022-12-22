8c30994d3844d2c4cd6e941030fed6ebb86b537d
bab882 <chanitebrandon@gmail.com>
2022-12-22 13:55:28 +0100

    first commit

--- faker.php
+++ faker.php
@@ -10,78 +10,94 @@ if (!$conn) {
     die("Erreur de connexion" . mysqli_connect_error());
 }
 
+mysqli_query($conn, "TRUNCATE `article` ");
+mysqli_query($conn, "TRUNCATE `blog` ");
+mysqli_query($conn, "TRUNCATE `carte` ");
+mysqli_query($conn, "TRUNCATE `carte_pic` ");
+
 require_once 'vendor/autoload.php';
 
 $faker = Faker\Factory::create();
-
-    /* 
-     *insert into catagory
-     * je dois mettre 5 categorie 
-     */
-    for( $c = 1; $c <= 5; $c++ ) {
-        $cat = 'INSERT INTO category( 
-            id,
-            title,
-            slug
-        ) 
-        VALUES(
-            '.$c.',
-            "'.$faker->words(3, true).'",
-            "'.$faker->slug().'"
-
-        )'; 
-        
-        if(mysqli_query($conn, $cat)){
-            echo 'save done !';
-        }
-        else {
-            echo 'error'.$sql.'<br>'.mysqli_error($conn);
-        } 
-        for($r = 1; $r <= 9; $r++){
-            $res = 'INSERT INTO reservation VALUES(
-                '.$r.',
-                '.$c.',
-                "'.$faker->name().'",
-                "'.$faker->firstName().'",
-                '.$faker->rand(1,26).',
-                "'.$faker->email().'",
-                "'.$faker->sentence().'",
-               " '.$faker->phoneNumber().'",
-                "'.$faker->paragraphs().'"
+//boucle table : article 
+for ($a = 1; $a < 6; $a++) {
+    $art = 'INSERT INTO article VALUES(
+        ' . $a . ',
+        ' . rand(1, 3) . ',
+        "' . $faker->sentence() . '",
+        "' . $faker->date('Y-m-d h:i:s') . '",
+        "' . $faker->date('Y-m-d h:i:s') . '",
+        "' . $faker->slug() . '"
+    )';
+    mysqli_query($conn, $art);
+}
+//boucle table : blog
+for ($b = 1; $b < 3; $b++) {
+    $blog = 'INSERT INTO blog VALUES(
+        ' . $b . ',
+        "' . $faker->name() . '",
+        "' . $faker->slug() . '"
+    )';
+    mysqli_query($conn, $blog);
+}
+//boucle table : carte
+for ($m = 1; $m < 5; $m++) {
+    $menu = 'INSERT INTO carte VALUES(
+                    ' . $m . ',
+                    "' . $faker->name() . '",
+                    "' . $faker->sentence() . '",
+                    "' . rand(1, 99) . '",
+                    "' . $faker->name() . '"                
+                )';
+    mysqli_query($conn, $menu);
+}
+// boucle table : carte_pic
+for ($p = 1; $p < 4; $p++) {
+    $pic = 'INSERT INTO carte_pic VALUES(
+                    ' . $p . ',
+                    ' . rand(1, 5) . ',
+                    "' . $faker->name() . '",
+                    "' . $faker->sentence() . '"
+                    )';
+    echo $pic;
+    mysqli_query($conn, $pic);
+}
+//boucle table : category
+for ($c = 1; $c < 3; $c++) {
+    $cat = 'INSERT INTO category VALUES(
+            ' . $c . ',
+            "' . $faker->sentence() . '",
+            "' . $faker->slug() . '"
             )';
-            if(mysqli_query($conn, $res)){
-                echo 'good reservation';
-            }
-            else {
-                echo 'error reservation'.$sql.'<br>'.mysqli_error($conn);
-            }
-        } 
-        for($m = 1; $m <= 7; $m++){
-            $menu = 'INSERT INTO carte VALUES(
-                '.$m.',
-                '.$c.',
-                "'.$faker->name().'" 
-                "'.$faker->sentence().'",
-                '.$faker->randomFloat(1, 20, 30).',
-                '.$faker->rand(1,8).',
-
-            )';   
-        }
-        for($b = 1; $b <= 4; $b++){
-            $blog = 'INSERT INTO blog VALUES(
-                '.$b.',
-                '.$c.',
-                "'.$faker->name().'",
+    mysqli_query($conn, $cat);
+}
+//boucle table : galery
+for ($g = 1; $g < 4; $g++) {
+    $gal = 'INSERT INTO galery VALUES(
+            ' . $g . ',
+            ' . rand(1, 3) . ',
+            "' . $faker->name() . '",
+            "' . $faker->sentence() . '"
+        )';
+    mysqli_query($conn, $gal);
+}
+//boucle table : reservation
+for ($r = 1; $r < 8; $r++) {
+    $re = 'INSERT INTO reservation VALUES(
+                ' . $r . ',
+                "' . $faker->name() . '",
+                "' . $faker->firstName() . '",
+                ' . rand(1, 10) . ',
+                "' . $faker->email() . '",
+                "' . $faker->sentence() . '",
+                " ' . $faker->phoneNumber() . '",
+                ' . rand(0, 1) . '
             )';
-        }   
-    }
-    
-    
-        
-    
-    
-    
+    mysqli_query($conn, $re);
+}
 
 
 
-?>
+
+
+
+mysqli_close($conn);
